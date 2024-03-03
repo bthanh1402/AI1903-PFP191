d73723c5cc11719de122cb9caf88c5819573ca92
bthanh1402 <155624790+bthanh1402@users.noreply.github.com>
2024-03-03 23:52:54 +0700

    Add files via upload

---
+++ PracticalSlot13/Q2.py
@@ -0,0 +1,14 @@
+#Q2.1
+numbers = [1,2,3,4,5,6,7]
+for i in range(len(numbers)):
+    numbers[i] = numbers[i]**2
+print(numbers)
+
+#Q2.2
+list1 = ['Hello ', 'take ']
+list2 = ['Dear', 'Sir']
+newlist = []
+for n in list1:
+    for m in list2:
+        newlist.append(n+m)
+print(newlist)
