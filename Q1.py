de6baeb99a34eb8fc7465508543ff5d95f8cc900
bthanh1402 <155624790+bthanh1402@users.noreply.github.com>
2024-03-03 23:25:22 +0700

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,14 @@
+#Q1.1
+lista = [1, 2 ,3]
+listb = [1.0, 'Jessa', 3]
+print(lista, listb)
+
+#Q1.2
+list1 = ['M','na','i','Ke']
+list2 = ['Y','me','s','lly']
+res = []
+for i in range(len(list1)):
+    res.append(list1[i] + list2[i])
+print(res)
+
+
